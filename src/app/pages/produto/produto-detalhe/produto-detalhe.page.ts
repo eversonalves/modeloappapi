56e7be234a135c6de3875fedc2d3cd201e750a07
Everson Reis <everson.alves18@hotmail.com>
2026-02-11 11:52:58 -0300

    finalizando produto.

--- src/app/pages/produto/produto-detalhe/produto-detalhe.page.ts
+++ src/app/pages/produto/produto-detalhe/produto-detalhe.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
+import { Vendas } from 'src/app/services/vendas';
 
 @Component({
   selector: 'app-produto-detalhe',
@@ -8,9 +12,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProdutoDetalhePage implements OnInit {
 
-  constructor() { }
+  id!: number;
+  form! : FormGroup;
+  categorias : any [] = [];
+  produto: any [] = [];
+
+  constructor(
+    private route: ActivatedRoute,
+    private formBuilder: FormBuilder,
+    private api: Vendas,
+    private router: Router,
+    private toast: ToastController
+  ) { }
 
   ngOnInit() {
+    this.id = Number(this.route.snapshot.paramMap.get('id'));
+    this.listaCategorias();
+    this.buscarProduto(this.id);
+  }
+
+  listaCategorias(){
+    this.api.operacao({requisicao:'categoria-listar'}).subscribe((res:any)=>{
+      if(res.success){
+        this.categorias = res.data;
+      }
+    })
+  }
+
+  buscarProduto(id:number){
+        this.api.operacao({requisicao:'produto-listar', id:this.id}).subscribe((res:any)=>{
+      if(res.success){
+        this.produto = res.data;
+      }
+    });
   }
 
 }
