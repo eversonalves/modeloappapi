a1fd0208b4a5a5daef3b5fb733803a1a6ce61fc2
Everson Reis <everson.alves18@hotmail.com>
2026-02-20 10:58:38 -0300

    att

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -14,7 +15,8 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [BrowserModule,
      IonicModule.forRoot(),
      HttpClientModule,
-     AppRoutingModule
+     AppRoutingModule,
+     ReactiveFormsModule
     ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
